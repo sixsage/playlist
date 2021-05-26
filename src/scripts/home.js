151a92c6b76b40a9a1eab86d77c90b55eb2fc860
sixsage <sixsage02@gmail.com>
2021-05-26 02:43:27 -0700

    First try to load next vid

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,23 +1,25 @@
 // 2. This code loads the IFrame Player API code asynchronously.
 function loadYoutubeAPI() {
-    const tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
+  const tag = document.createElement("script");
+  tag.src = "https://www.youtube.com/iframe_api";
 
-    const firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-    tag.onload = onYouTubeIframeAPIReady();
+  const firstScriptTag = document.getElementsByTagName("script")[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  //tag.onload = onVideoAPIReady;
 }
 loadYoutubeAPI();
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
 
-function onVideoAPIReady() {
+var player
+function onVideoAPIReady(videoID) {
     window.YT.ready(() => {
-        let player = new window.YT.Player('video', {
-            videoId: 'M7lc1UVf-VE',
+        player = new window.YT.Player('video', {
+            videoId: videoID,
             events: {
               'onReady': onPlayerReady,
+              'onStateChange': onVideoStateChange          
             }
         });
     });
@@ -28,3 +30,36 @@ function onPlayerReady(event) {
   event.target.playVideo();
 }
 
+function getVideoID(videoURL) {
+  let videoID;
+  if (videoURL.indexOf("=") != -1) {
+    videoID = videoURL.substring(videoURL.indexOf("=") + 1);
+  }
+  return videoID;
+}
+
+var idArr = [];
+
+function enterToAddVideoToQueue() {
+  const linkInput = document.getElementById("link-input");
+
+  linkInput.addEventListener("keypress", (e) => {
+    let key = e.which || e.keyCode;
+
+    if (key === 13) {
+      idArr.push(getVideoID(linkInput.value));
+      console.log(idArr);
+      onVideoAPIReady(idArr[0]); //this plays the first video
+    }
+  });
+}
+enterToAddVideoToQueue();
+
+function onVideoStateChange(event) {
+  if (event.data == 0) { //when the video ends
+    console.log(12);
+    idArr.splice(0, 1);
+    console.log(idArr);
+    player.loadVideoById(idArr[0]);
+  }
+}
